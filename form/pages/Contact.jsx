2b5c5465958befcdbd960e4bbb0d8bb6c387af78
Code-war307 <uttampradhan115@gmail.com>
2025-11-03 15:45:09 +0530

    deployed

---
+++ form/pages/Contact.jsx
@@ -0,0 +1,85 @@
+import { useState } from 'react';
+import './Contact.css';
+import toast from 'react-hot-toast';
+
+const Contact = () => {
+  const [formData, setFormData] = useState({ name: '', email: '' });
+  const [errors, setErrors] = useState({});
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const validate = () => {
+    const newErrors = {};
+    if (!formData.name.trim()) newErrors.name = 'Name is required';
+    if (!formData.email.trim()) {
+      newErrors.email = 'Email is required';
+    } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(formData.email)) {
+      newErrors.email = 'Invalid email format';
+    }
+    return newErrors;
+  };
+
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+    const validationErrors = validate();
+    if (Object.keys(validationErrors).length > 0) {
+      setErrors(validationErrors);
+    } else {
+      let response = await fetch('http://localhost:3000/api/form', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      let backendResponse = await response.json();
+      
+      if(backendResponse.success){
+        toast.success(backendResponse.message);
+      }
+      else{
+        toast.error('Form submission failed. Please try again.');
+      }
+      setFormData({ name: '', email: '' });
+      setErrors({});
+      
+    }
+  };
+
+  return (
+    <div className="contact-form-container">
+      <h2>Contact Form</h2>
+      <form onSubmit={handleSubmit} className="contact-form">
+        <div className="form-group">
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+          />
+          {errors.name && <span className="error">{errors.name}</span>}
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+          {errors.email && <span className="error">{errors.email}</span>}
+        </div>
+        <button type="submit" className="submit-button">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default Contact;
